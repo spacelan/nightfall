3f643cd442c3a1c0cf2a399d1714fdc3dbab3e59
Snow.Hellsing <snow.hellsing@gmail.com>
2011-09-07 00:06:12 +0800

    load topic preview on demand

--- inject.js
+++ inject.js
@@ -4,6 +4,8 @@
 
 (function($)
 {
+  //  declearing vars and funcs
+  // ------------------
   var blackList = localStorage['snowhs/nightfall'],
     
     // constants
@@ -12,10 +14,13 @@
     $topicLs = $('.olt'),
     
     // dom templates 
-    $oprtsTpl = $('<div class="ntf-oprts"><a class="ntf-preview">preview</a>'+
-      '<a class="ntf-hide">hide</a><a class="ntf-mute">mute</a></div>'),
-    $previewTpl = $('<tr class="ntf-preview"><td colspan="5"></td></tr>');
-    
+    $oprtsTpl = $('<div class="ntf-oprts"><a class="ntf-preview" href="javascript:void(0)">preview</a>'+
+      '<a class="ntf-enter" href="javascript:void(0)">enter</a>'+
+      '<a class="ntf-hide" href="javascript:void(0)">hide</a>'+
+      '<a class="ntf-mute" href="javascript:void(0)">mute</a></div>'),
+    $previewTpl = $('<tr class="ntf-preview ntf-empty"><td colspan="5">loading...</td></tr>');
+  
+  
   function extractTopicId(uri)
   {
     return uri.slice(34, -1);
@@ -39,7 +44,9 @@
     $li.next('tr.ntf-preview').remove();
     $li.remove();        
   }
-    
+  
+  //  let's jean!
+  // -------------
   if('string' === typeof blackList)
   {
     blackList = blackList.split(',');
@@ -49,6 +56,8 @@
     blackList = [];
   }
   
+  //  process topic list
+  // --------------------
   $topicLs.find('tr.pl').each(function(idx, el)
   {
     var $li = $(this),
@@ -57,8 +66,9 @@
     
     if(-1 === $.inArray(id, blackList))
     {
-      $li.find('td:nth-child(5)').append($oprtsTpl.clone()).
-        end().after($previewTpl.clone().find('td').load(uri+' .topic-content').end());
+      $li.find('td:nth-child(5)').
+        append( $oprtsTpl.clone().find('.ntf-enter').attr('href', uri).end() ).
+        end().after( $previewTpl.clone() );
     }
     else
     {
@@ -77,11 +87,79 @@
     }
     else if($t.hasClass('ntf-preview'))
     {
-      $li.addClass('on').next('tr.ntf-preview').show('fast');
+      var $preview = $li.addClass('on').next('tr.ntf-preview').show('fast');
+      
+      if($preview.hasClass('ntf-empty'))
+      {
+        $preview.find('td').load($li.find('td:first a').attr('href')+' .topic-content', function()
+        {
+          $(this).find('.topic-opt, .sns-bar').remove();
+          $preview.removeClass('ntf-empty');
+        });
+      }
     } 
     else if($t.hasClass('ntf-hide'))
     {
       $li.removeClass('on').next('tr.ntf-preview').hide('fast');
     }       
+  });
+  
+  // inject new member tab
+  // ----------------------
+  var $tabNav = $('.zbar>div'),
+    $pgMain = $topicLs.closest('.indent'),
+    $memberLs = $('<div id="ntf-memberLs">'),
+    $memberTabHandler = $tabNav.find('a:first').clone().
+      attr('href','javascript:void(0)').attr('id','#ntf-memberTabHandler').text('Members').
+      appendTo($tabNav),
+    $topicTabHandler = $memberTabHandler.clone().
+      addClass('on').attr('id','#ntf-topicTabHandler').text('Topics').
+      replaceAll($tabNav.find('.now'));
+      
+  //function     
+      
+  $memberTabHandler.click(function()
+  {    
+    $topicLs.detach();
+    $memberLs.appendTo($pgMain);
+    $memberTabHandler.addClass('on');
+    $topicTabHandler.removeClass('on');
+  });    
+  
+  $topicTabHandler.click(function()
+  {
+    $memberLs.detach();
+    $topicLs.appendTo($pgMain);
+    $topicTabHandler.addClass('on');
+    $memberTabHandler.removeClass('on');
+  });
+  return;
+  var members = [],
+    $userLiTpl;
+  
+  $('<div>').load('/contacts/list .user-list', function()
+  {
+    var $t = $(this);
+    $userLiTpl = $t.find('li:first');
+    $t.empty().appendTo($memberLs);
+  });
+  
+  $('<div />').load('/group/mine .indent:last', function()
+  {
+    $.each($(this).find('dl'), function(idx, groupLi)
+    {
+      $('<div />').load( $(groupLi).find('a:first').attr('href')+'members .obss:last', function()
+      {
+        $.each($(this).find('dl'), function(idx, memberLi)
+        {
+          var href = $(this).find('a:first').attr('href');
+          console.log(href);
+          if(-1 === $.inArray(href, members))
+          {
+            //members.push(href);
+          }
+        });
+      });
+    });
   });
 })(jQuery);
